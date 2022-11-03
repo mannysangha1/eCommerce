5f7f45a701f2fdfb0707812b2ab96b5436a03e7d
Manny <mannysangha020@gmail.com>
2022-11-03 03:40:41 -0700

    added code to router.get

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,9 +9,20 @@ router.get('/', (req, res) => {
   Category.findAll({
     include: {
       model: Product,
-      attributes: []
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
     }
   })
+  .then(dbCatData => {
+    if(!dbCatData) {
+      res.status(404).json({message: 'No categories found'});
+      return;
+    }
+    res.json(dbCatData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.get('/:id', (req, res) => {
